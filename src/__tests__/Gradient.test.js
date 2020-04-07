9a1fcf753b20069aef4686a745312a27871de0c6
Li-Hsuan <lihsuanlung@gmail.com>
2020-04-07 13:55:57 -0400

    tmp

--- src/__tests__/Gradient.test.js
+++ src/__tests__/Gradient.test.js
@@ -1,5 +1,6 @@
 import Jimp from 'jimp';
 import 'regenerator-runtime/runtime';
+import fs from 'fs';
 import Gradient from '../Gradient';
 
 describe(Gradient, () => {
@@ -10,15 +11,66 @@ describe(Gradient, () => {
     });
 
     it('creates gradient for 1 pixel', async () => {
-      const image = await Jimp.read('./src/__tests__/examples/9.jpg');
-      const { width, height } = image._exif.imageSize;
-      [...Array(width).keys()].forEach((x) => {
-        [...Array(1).keys()].forEach((y) => {
+      // const image = await Jimp.read('./src/__tests__/examples/9.jpg');
+      const image = await Jimp.read('./src/__tests__/examples/10.png');
+      const { width, height } = image.bitmap || image._exif.imageSize;
+      const gradients = [];
+      [...Array(height).keys()].forEach((y) => {
+        const colors = [];
+        [...Array(width).keys()].forEach((x) => {
           const color = image.getPixelColor(x, y);
           const rgba = Jimp.intToRGBA(color);
-          console.log(rgba);
+          colors.push(rgba);
         });
+        const pixels = colors.map((rgba) => ({
+          r: rgba.r,
+          g: rgba.g,
+          b: rgba.b,
+          a: rgba.a,
+          start: 0,
+          end: 1,
+        }));
+
+        const compressed = [];
+        let prevPixel = null;
+
+        pixels.forEach((pixel) => {
+          prevPixel = compressed[compressed.length - 1];
+
+          if (prevPixel) {
+            if (pixel.r === prevPixel.r
+              && pixel.g === prevPixel.g
+              && pixel.b === prevPixel.b
+              && pixel.a === prevPixel.a
+            ) {
+              prevPixel.end += 1;
+            } else {
+              pixel.start = prevPixel.end;
+              pixel.end = prevPixel.end + 1;
+              compressed.push(pixel);
+            }
+          } else {
+            compressed.push(pixel);
+          }
+        });
+
+        if (compressed.length === 1) {
+          compressed.push({
+            r: prevPixel.r,
+            g: prevPixel.g,
+            b: prevPixel.b,
+            a: prevPixel.a,
+            start: 0,
+            end: width,
+          });
+        }
+
+        const rgbas = compressed.map((pixel) => `rgba(${pixel.r}, ${pixel.g}, ${pixel.b}, ${pixel.a / 255}) ${pixel.start}px ${pixel.end}px`);
+        const gradient = `linear-gradient(to right, ${rgbas.join(', ')})`;
+        gradients.push(gradient);
       });
+      const divs = gradients.map((gradient) => `<div style="width: ${width}px; height: 1px; background: ${gradient};"></div>`);
+      fs.writeFileSync('test.html', divs.join('\n'));
     });
   });
 });
